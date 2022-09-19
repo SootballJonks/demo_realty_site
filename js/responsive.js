6fddcaf8a53daf81afcfd17c71f26e8e95d38e3b
Jonks <anastasiablackhold@gmail.com>
2022-09-19 01:29:24 -0700

    featurette animations added to marketing page

--- js/responsive.js
+++ js/responsive.js
@@ -36,3 +36,27 @@ window.addEventListener('resize', () => {
 }
 
 //---------------------- ABOUT ----------------------
+
+// Put something here...
+
+//---------------------- REAL ESTATE MARKETING ----------------------
+
+if (document.location.pathname === "/html/real-estate-marketing.html") {
+  function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
+  
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+}
